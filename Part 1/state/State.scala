fadd8d1306bf8200b6777533af4d49bc8f2e2426
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-02-06 17:56:35 +0100

    state ex 6.1

---
+++ Part 1/state/State.scala
@@ -0,0 +1,39 @@
+def rollDie: Int = {
+  val rng = new scala.util.Random
+  rng.nextInt(6)
+}
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+
+  override def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+// hypothetical code
+class Foo {
+  private var s: FooState = ???
+  def bar: Bar = ???
+  def baz: Int = ???
+}
+
+
+def randomPair(rng: RNG): (Int, Int) = {
+  val (a, rng2) = rng.nextInt
+  val (b, _) = rng2.nextInt
+  (a, b)
+}
+
+def nonNegativeInt(rng: RNG): (Int, RNG) = {
+  val (a, rng2) = rng.nextInt
+  if(a >= 0 && a < Int.MaxValue) (a, rng2)
+  else nonNegativeInt(rng2)
+}
