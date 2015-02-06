06084e3cc810c99b31ffdbe95aa763b10cacea65
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-02-06 19:24:45 +0100

    map funtion

--- Part 1/state/State.scala
+++ Part 1/state/State.scala
@@ -78,7 +78,10 @@ type Rand[+A] = RNG => (A, RNG)
 
 val int: Rand[Int] = _.nextInt
 def unit[A](a: A): Rand[A] = rng => (a, rng)
-
+def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+  val (a, rng2) = s(rng)
+  (f(a), rng2)
+}
 
 
 
