f1d379fd0bea518b5e7832d93c87af718623970f
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-02-08 18:39:45 +0100

    state ex 6.9 _map2

--- Part 1/state/State.scala
+++ Part 1/state/State.scala
@@ -111,7 +111,7 @@ def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
 // 6.9
 def _map[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => rng => (f(a), rng))
 def _map[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
-
+def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] = flatMap(ra)(a => rb.map(b => f(a,b)))
 
 
 
