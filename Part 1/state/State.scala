35575ef1675ea3533e2e9fe1cb9ac6b99af84574
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-02-08 17:00:30 +0100

    state ex 6.6

--- Part 1/state/State.scala
+++ Part 1/state/State.scala
@@ -77,13 +77,22 @@ def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
 type Rand[+A] = RNG => (A, RNG)
 
 val int: Rand[Int] = _.nextInt
+
 def unit[A](a: A): Rand[A] = rng => (a, rng)
+
 def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
   val (a, rng2) = s(rng)
   (f(a), rng2)
 }
 
+// 6.5 - TODO
 
+// 6.6
+def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+  val (a, rng2) = ra(rng)
+  val (b, rng3) = rb(rng2)
+  (f(a,b), rng3)
+}
 
 
 
