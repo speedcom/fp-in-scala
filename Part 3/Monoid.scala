c34b1cee4dac656a31956151685b664bd66085aa
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-04 22:20:43 +0100

    ex 10.3

--- Part 3/Monoid.scala
+++ Part 3/Monoid.scala
@@ -25,18 +25,40 @@ val intMultiplication = new Monoid[Int] {
   def zero = 1
 }
 
-val booleanOr: Monoid[Boolean] {
+val booleanOr = new Monoid[Boolean] {
   def op(i1: Boolean, i2: Boolean) = i1 || i2
   def zero = false
 }
 
-val booleanAnd: Monoid[Boolean] {
+val booleanAnd = new Monoid[Boolean] {
   def op(i1: Boolean, i2: Boolean) = i1 && i2
   def zero = true
 }
 
 // EX 10.2
-def optionMonoid[A]: Monoid[Option[A]] = {
+def optionMonoid[A] = new Monoid[Option[A]] = {
   def op(op1: Option[A], op2: Option[A]) = op1 orElse op2
   def zero = None
 }
+
+// EX 10.3
+// A function having the same argument and return type is sometimes called an endofunction.
+// This is a monoid for endofunctions.
+def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+  def op(e1: A => A, e2: A => A): A => A = a => e1 compose e2
+  def zero: A => A = a => a
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
