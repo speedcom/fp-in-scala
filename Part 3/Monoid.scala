426796203035a2945978d06b9110cc032d02f65d
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-08 13:56:30 +0100

    toList foldable

--- Part 3/Monoid.scala
+++ Part 3/Monoid.scala
@@ -89,6 +89,7 @@ trait Foldable[F[_]] {
   def foldLeft[A,B](as: F[A])(z: B)(f: (B,A) => B): B
   def foldMap[A,B](as: F[A])(f: A => B)(mb: Monoid[B]): B
   def concatenate[A](as: F[A])(m: Monoid[A]): A = foldLeft(as)(m.zero)(m.op)
+  def toList[A](fa: F[A]): List[A] = foldLeft(fa)(List[A]())((l, a) => a :: l)
 }
 
 // EX 10.12
