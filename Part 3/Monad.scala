3a29886cf2e174c97eaf348e56aa832825dbf96d
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-09 19:58:22 +0100

    changed name file

---
+++ Part 3/Monad.scala
@@ -0,0 +1,25 @@
+trait Functor[F[_]] {
+
+  def map[A,B](as: F[A])(f: A => B): F[B]
+
+  def distribute[A,B](as: F[(A,B)]): (F[A], F[B]) = (map(as)(_._1), map(as)(_._2))
+
+  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa) => map(fa)(Left(_))
+    case Right(fb) => map(fb)(Right(_))
+  }
+}
+
+object Functor {
+  val listFunctor = new Functor[List] {
+    def map[A,B](as: List[A])(f: A => B): List[B] = as map f
+  }
+}
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A,B](fa: F[A])(f: A => F[B]): F[B]
+  def map[A,B](fa: F[A])(f: A => B): F[B] = flatMap(fa)(a => unit(f(a)))
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] = flatMap(fa)(a => map(fb)(b => f(a,b)))
+}
+
