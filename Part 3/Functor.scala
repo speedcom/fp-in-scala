37a4c48c42311a6aca980928a9e1093eb572d708
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-09 19:13:30 +0100

    codistribute method of functor

--- Part 3/Functor.scala
+++ Part 3/Functor.scala
@@ -1,6 +1,13 @@
 trait Functor[F[_]] {
+
   def map[A,B](as: F[A])(f: A => B): F[B]
+
   def distribute[A,B](as: F[(A,B)]): (F[A], F[B]) = (map(as)(_._1), map(as)(_._2))
+
+  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa) => map(fa)(Left(_))
+    case Right(fb) => map(fb)(Right(_))
+  }
 }
 
 object Functor {
@@ -8,5 +15,3 @@ object Functor {
   val listFunctor = new Functor[List] {
     def map[A,B](as: List[A])(f: A => B): List[B] = as map f
   }
-
-}
