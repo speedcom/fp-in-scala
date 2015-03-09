7f3a18d91884f7abd1409da73100ec67853f1603
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-09 19:39:37 +0100

    monad interface

--- Part 3/Functor.scala
+++ Part 3/Functor.scala
@@ -11,7 +11,14 @@ trait Functor[F[_]] {
 }
 
 object Functor {
-
   val listFunctor = new Functor[List] {
     def map[A,B](as: List[A])(f: A => B): List[B] = as map f
   }
+}
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A,B](fa: F[A])(f: A => F[B]): F[B]
+  def map[A,B](fa: F[A])(f: A => B): F[B] = flatMap(fa)(a => unit(f(a)))
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] = flatMap(fa)(a => map(fb)(b => f(a,b)))
+}
