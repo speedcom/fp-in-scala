d94d99f3f0c306ee4a4fe15cfb19737d4e223854
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-12 22:31:41 +0100

    monad sequence combinator

--- Part 3/Monad.scala
+++ Part 3/Monad.scala
@@ -21,8 +21,11 @@ trait Monad[F[_]] extends Functor[F] {
   def flatMap[A,B](fa: F[A])(f: A => F[B]): F[B]
   def map[A,B](fa: F[A])(f: A => B): F[B] = flatMap(fa)(a => unit(f(a)))
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] = flatMap(fa)(a => map(fb)(b => f(a,b)))
+  def sequence[A](lma: List[F[A]]): F[List[A]] = {
+    lma.foldRight(unit(List[A]()))((fa, fla) => map2(fa, fla)(_ :: _))
+  }
 }
 
-def sequence[A](lma: List[F[A]]): F[List[A]]
-def traverse[A,B](la: List[A])(f: A => F[B]): F[List[B]]
+
+def traverse[A,B](la: List[A])(f: A => F[B]): F[List[B]] = ???
 
