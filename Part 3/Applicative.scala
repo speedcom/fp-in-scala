432635923d206e3436637d2ac13b419791d2bbc7
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-16 22:52:58 +0100

    Making Monad a subtype of Applicative

--- Part 3/Applicative.scala
+++ Part 3/Applicative.scala
@@ -7,7 +7,22 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+
   def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
+
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
+
   def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] = map2(fa, fb)((_,_))
+
+  def map3[A,B,C,D](fa: F[A],
+                    fb: F[B],
+                    fc: F[C])(f: (A, B, C) => D): F[D] = ???
+}
+
+trait Monad[F[_]] extends Applicative[F] {
+  def flatMap[A,B](fa: F[A])(f: A => F[B]): F[B] = join(map(fa)(f))
+  def join[A](ffa: F[F[A]]): F[A] = flatMap(ffa)(fa => fa)
+  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] = a => flatMap(f(a))(g)
+  def map[B](fa: F[A])(f: A => B): F[B] = flatMap(fa)((a: A) => unit(f(a)))
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = flatMap(fa)(a => map(fb)(b => f(a,b)))
 }
