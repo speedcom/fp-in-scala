f6efa8c7e6ea586ee28bd4e2d8c82754eaa10f6e
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-16 22:34:35 +0100

    Applicative map in terms of unit and apply

--- Part 3/Applicative.scala
+++ Part 3/Applicative.scala
@@ -1,13 +1,12 @@
 trait Applicative[F[_]] extends Functor[F] {
-  // primitive combinators
-  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B]
   def unit[A](a: => A): F[A]
 
-  // derived combinators
-  def map[A,B](fa: F[A])(f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
+  def map[A,B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] = ???
+
   def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
-
   def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
   def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] = map2(fa, fb)((_,_))
