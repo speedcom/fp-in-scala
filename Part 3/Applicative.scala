e306bfae9c007f039fefa8b3c595b3d541605cce
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-17 16:08:45 +0100

    validation-{failure,success} sceleton

--- Part 3/Applicative.scala
+++ Part 3/Applicative.scala
@@ -39,3 +39,14 @@ object Applicative {
       a.foldRight(unit(List[A]()))((sa, l) => map2(sa, l)(_ :: _))
   }
 }
+
+sealed trait Validation[+E, +A]
+
+case class Failure[E](head: E, tail: Vector[E] = Vector()) extends Validation[E, Nothing]
+
+case class Success[A](a: A) extends Validation[Nothing, A]
+
+
+
+
+
